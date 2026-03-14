8060cb10457342eb5b7cbd3e421f4f612092d2dd
ibrohim-265 <ir4891944@gmail.com>
2026-03-14 17:18:55 +0500

    python lessons

---
+++ uyga vazifa.py
@@ -0,0 +1,38 @@
+# ona = {
+#     'ism': "Sadoqat",
+#     'tugilgan_yil': 1985,
+#     'shahar': "xorazm"
+# }   
+# print(f"Onamning ismi {ona['ism']}, {ona['tugilgan_yil']}-yilda, {ona['shahar']} viloyatida tug'ilgan")
+
+# taomlar = {
+#     'ibrohim': "osh",
+#     'behruz': "manti",
+#     'elbek': "shashlik",
+# }
+# print(f"Ibrohimning sevimli taomi {taomlar['ibrohim']}")
+# print(f"Behruzning sevimli taomi {taomlar['behruz']}")
+# print(f"Elbekning sevimli taomi {taomlar['elbek']}")
+
+
+# lugat = {
+#     'integer': "butun son",
+#     'float': "o'nlik son",
+#     'string': "matn",
+#     'if': "agar",
+#     'else': "aks holda",
+#     'list': "ro'yxat",
+#     'tuple': "o'zgarmas ro'yxat",
+#     'dictionary': "lug'at",
+#     'boolean': "mantiqiy qiymat",
+#     'for': "uchun"
+# }
+
+
+# soz = input("Tarjimasini bilmoqchi bo'lgan so'zni kiriting: ")
+# tarjima = lugat.get(soz)
+# if tarjima:
+#     print(f"{soz}ning tarjimasi: {tarjima}")
+# else:
+#     print("Bunda so'z mavjud emas")
+
