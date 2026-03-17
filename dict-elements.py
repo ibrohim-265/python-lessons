6fd50cd23e427ccf529417068790b0245771e2dc
ibrohim-265 <ir4891944@gmail.com>
2026-03-17 17:47:35 +0500

    Add new examples for dictionary methods in DICTIONARY.py and create dict-elements.py for additional dictionary operations

--- dict-elements.py
+++ dict-elements.py
@@ -1,57 +1,3 @@
- # data typs
- # 1.string 2.integer  3.float 4.boolean 5.list 6.tuple  7.dictionary
-
- # dictionary
-# cars = ['audi', 'bmw', 'volvo']
-# car = {'brad':"ford",
-#        'model':"mustang",
-#        'year':1964,
-#        'color':"red"}
-# print(car)
-# print(type(car))
-
-
-# student = {
-#     'fullname': "Elbek",
-#     'age': 20,
-#     'course': 3,
-#     'favorite_books': ["atomic habits", "otkan kunlar", "dunyoning ishlari"],
-#     'is_coplete_books': False,
-#     'is_married': True
-# }
-# print(student)
-
-
- # 1. QIYMATLARNI OLISH
-# print(student['fullname'])
-# print(student['age'])   
-# print(student['favorite_books']) 
-
-# for book in student['favorite_books']:
-#     print(book)
-
- # 2. qiymatlarni ozgatirish
-# student['age'] = 21
-# student['course'] = 4
-# print(student)
-
- # 3.yangi key_value qo'shish
-# student['hobby'] = ["football", "programming", "traveling", "cooking"]
-# print(student)
-
- # 4. key-value ochirish
-# del student['is_married']
-# print(student)
-
- # 5. Empty dictionarys
-# talaba_1 = {}
-
-# talaba_1['ism'] = 'qobil rasulov'
-# talaba_1['kurs'] = 3
-# talaba_1['yosh'] = 20
-# print(talaba_1)
-# print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs")
-
 # Dictionary elementlari bilan ishlash
 phone = {
     'brand': "Iphone",
@@ -100,3 +46,16 @@ fruist = ['olma', 'anor', 'uzum', 'anjir', 'shaftoli']
 print('olma' in fruist) # True
 print('tarvuz' in fruist) # False
 
+fruit = input("Qaysi meva yoqadi? ")
+if fruit in fruist:
+    print(f"{fruit.title()} do'konimizda bor")
+else:
+    print(f"{fruit.title()} do'konimizda yo'q")
+
+bozorlik = ['anor','uzum','non','baliq']
+for mahsulot in mahsulotlar:
+    print(mahsulot) # lug'atning kalitlari bo'ladi    
+# mahsulotlar - lug'at, bozorlik - ro'yxat, mahsulot - lug'atning  kaliti
+for mahsulot in mahsulotlar:
+    if mahsulot in bozorlik:
+        print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
