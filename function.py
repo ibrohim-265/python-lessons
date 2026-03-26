39d5ed7b6d2c6952f909a8f2bb7a5c72bdb80596
ibrohim-265 <ir4891944@gmail.com>
2026-03-26 18:04:43 +0500

    Add function examples to demonstrate function creation and calling

---
+++ function.py
@@ -0,0 +1,37 @@
+ # Function - ma'lum vazifani bajaruchi kod bloklari
+ # Funksiya yaratish uchun def kalit so'zidan foydalanamiz
+# print("Hello, World!")
+ # Funksiyani e'lon qilish(declaration)
+# def salom_ber():
+#     print("Salom, Dunyo!")
+
+ # Funksiyani chaqirish (call)
+# salom_ber() # Natija: Salom, Dunyo!
+
+ # Funksiya parametrlar , argumentlar 
+# def salom_ber(ism):
+#     print(f"Salom, {ism}!")
+
+# salom_ber("Ali") 
+# salom_ber("Vali")
+# salom_ber("Olim")   
+# salom_ber(" Gulbahor ammam")
+
+# def yigindi(a, b):
+#     print(a + b)
+
+# yigindi(7, 8) # Natija: 15
+# yigindi(10, 20)  # Natija: 30
+
+# def calculate_age(birth_year=1995, name= "Sobirjon"):
+#     age = 2026 - birth_year
+#     print(f"{name}ning yoshi: {age}")
+
+# calculate_age(2005, "Sobirjon")    
+# calculate_age(2007, "Sulaymon")
+#calculate_age()
+
+# def yosh_hisobla(joriy_yil, joriy_yil=2020): # joriy yil uchun st.qiymat 2020
+#     print(f"siz {joriy_yil-tugilgan_yil} yoshdasiz")
+
+# yosh_hisobla(1995, 2020)
